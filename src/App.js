fbe21c22bd93b2a6104ecec3e3f6abbe54baeda3
MIN60 <49427080+MIN60@users.noreply.github.com>
2022-01-13 21:30:39 +0900

    Add Swiper

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "./App.css";
 
-function App() {
+SwiperCore.use([EffectCoverflow, Pagination]);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          안녕하세요~ 새 branch입니다
-        </p>
-      
-      </header>
+    <div>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        pagination={true}  //아래에 현재 슬라이드 이미지 위치 표시하는 인디케이터
+        paginationClickable={true} //인디케이터 클릭
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" alt="" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
-}
+};
 
 export default App;
