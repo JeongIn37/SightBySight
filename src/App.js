eb652d76004d2dd2bd9a9ee8769ed40450635ecb
MIN60 <49427080+MIN60@users.noreply.github.com>
2022-01-14 20:57:58 +0900

    "Merge All"

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import SwiperPage from './pages/SwiperPage.js';
-
-function App() {
-  return (
-    <>
-    <div>
-    <Router>
-      <Routes>
-        <Route path='/' element={<SwiperPage/>}></Route>
-      </Routes>
-    </Router>
-    </div>
-    <div>
-      <p>여기다 좌석 넣으려고여</p>
-//고치기
-import React, {useState} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Review } from './review/index.js';
 import { Home } from './home/index.js';
 import { SignUp } from './signUp/index.js';
 import { Header } from './header/index.js';
 import { Profile } from './profile/index.js';
 import { WriteReview } from './review/index.js'
+import SwiperPage from './pages/SwiperPage.js';
 
 
 function App() {
@@ -41,13 +24,12 @@ function App() {
               <Route path='/MyPage' element={<Profile />}></Route>
               <Route path='/ReadReview' element={<Review />}></Route>
               <Route path='/WriteReview' element={<WriteReview />}></Route>
+              <Route path='/Seats' element={<SwiperPage/>}></Route>
           </Routes>
         </Router>
       </div>
 
-    </div>
-    </>
-    
+    </div>    
   );
 }
 
