1289c951364a7caf228319817294c7848d3e9250
jjminsuh <minsuh3203@gmail.com>
2022-01-14 07:55:59 +0000

    Add: sign up page and linked my page

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Review } from './review/index.js';
 import { Home } from './home/index.js';
 import { SignUp } from './signUp/index.js';
+import { Header } from './header/index.js';
+import { Profile } from './profile/index.js';
 
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Header
-        </p>
-      
-      </header>
-
       <div>
         <Router>
+          <div className='headerMain'>
+            <Header />
+          </div>
           <Routes>
               <Route exact path='/' element={<Home />}></Route>
-              <Route path='/SignUp' element={<SignUp />} />
+              <Route path='/SignUp' element={<SignUp />}></Route>
+              <Route path='/MyPage' element={<Profile />}></Route>
           </Routes>
         </Router>
       </div>
---
+++ src/profile/Profile.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Profile extends Component{
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+        
+        return (
+            <div>
+                My page
+            </div>
+        )
+    }
+}
+
+export default Profile;
